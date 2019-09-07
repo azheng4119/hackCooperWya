8185c90ad02cedcba8f5b481d4de5d8efd5890d6
NapatKarm <napat.karm@gmail.com>
2019-09-06 23:55:50 -0400

    redux

--- components/login.js
+++ components/login.js
@@ -1,31 +1,63 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
+import { connect } from "react-redux";
+import { loginThunk, getUserThunk} from '../store/utilities/currentuser';
 import { Button } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
 
-export default class Login extends React.Component {
+class Login extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            password: '',
+            username: '',
+        }
     }
-    handleChange = e => {
+    async componentDidMount(){
+
+    }
+    submit = () => {
+        const user = {
+            "username": this.state.username,
+            "password": this.state.password
+        }
+        this.props.login(user);
+    }
+
+    random = () =>{
+        try{
+            this.props.getUser();
+            console.log(this.props.user)
+        }catch (err){
+            console.log(err);
+        }
 
     }
     render() {
         return (
-            <View style = {styles.container}>
+            <View style={styles.container}>
                 <Text>Wya</Text>
                 <FormLabel><Text>Username</Text></FormLabel>
-                <FormInput inputStyle={{width : 200}}/>
+                <FormInput onChangeText={(text) => this.setState({
+                    username: text
+                })} inputStyle={{ width: 200 }} />
 
                 <FormLabel><Text>Password</Text></FormLabel>
-                <FormInput inputStyle={{width : 200}} />
+                <FormInput onChangeText={(text) => this.setState({
+                    password: text
+                })} inputStyle={{ width: 200 }} />
 
-                <View style={{margin : 10, alignItems : 'center',textAlign : 'center'}}>
-                    <Button style = {{width : '50%'}}
-                        onPress={() => Actions.home()}
-                    ><Text style = {{padding : 10, color:'white'}}>Login</Text></Button>
+                <View style={{ margin: 10, alignItems: 'center', textAlign: 'center' }}>
+                    <Button style={{ width: '50%' }}
+                        onPress={() => this.submit()}
+                    ><Text style={{ padding: 10, color: 'white' }}>Login</Text></Button>
+                </View>
+                <View style={{ margin: 10, alignItems: 'center', textAlign: 'center' }}>
+                    <Button style={{ width: '50%' }}
+                        onPress={() => this.random()}
+                    ><Text style={{ padding: 10, color: 'white' }}>test</Text></Button>
                 </View>
 
             </View>
@@ -35,9 +67,24 @@ export default class Login extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        display : 'flex',
+        display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
     },
 });
+
+const mapState = (state) => {
+    return {
+        routes: state.user
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return {
+        getUser: () => dispatch(getUserThunk()),
+        login: (user) => dispatch(loginThunk(user))
+    }
+}
+
+export default connect(mapState, mapDispatch)(Login);
--- pages/register.js
+++ pages/register.js
@@ -1,24 +1,72 @@
 import React from 'react'
-import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import axios from 'axios'
+import { StyleSheet, View, ScrollView, Text, TouchableOpacity } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
+
 
 export default class app extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstname: '',
+            firstlast: '',
+            password: '',
+            username: '',
+        }
+    }
+    handleChangefirstname = (event) => {
+        this.setState({ firstname: event.target.value })
+    }
+    handleChangelastname = (event) => {
+        this.setState({ lastname: event.target.value })
+    }
+    handleChangepassword = (event) => {
+        this.setState({ password: event.target.value })
+    }
+    handleChangeusername = (event) => {
+        this.setState({ username: event.target.value })
+    }
+    signUp = async () => {
+        try {
+            console.log(this.state)
+            await axios.post(`https://wya-api.herokuapp.com/user/signup`, {
+                "firstname": this.state.firstname,
+                "lastname": this.state.lastname,
+                "username": this.state.username,
+                "password": this.state.password
+            })
+            Actions.landing();
+        }
+        catch (authError) {
+            console.log(authError, "AN ERROR OCCURED")
+        }
+    }
     render() {
         return (
             <ScrollView>
-            <View style={styles.container}>
+                <View style={styles.container}>
                     <FormLabel><Text>First Name</Text></FormLabel>
-                    <FormInput />
+                    <FormInput onChangeText={(text) => this.setState({
+                        firstname: text
+                    })} />
                     <FormValidationMessage>Error message</FormValidationMessage>
                     <FormLabel><Text>Last Name</Text></FormLabel>
-                    <FormInput />
+                    <FormInput onChangeText={(text) => this.setState({
+                        firstlast: text
+                    })} />
                     <FormValidationMessage>Error message</FormValidationMessage>
                     <FormLabel><Text>Username</Text></FormLabel>
-                    <FormInput />
+                    <FormInput onChangeText={(text) => this.setState({
+                        username: text
+                    })} />
                     <FormValidationMessage>Error message</FormValidationMessage>
                     <FormLabel><Text>Password</Text></FormLabel>
-                    <FormInput/>
+                    <FormInput onChangeText={(text) => this.setState({
+                        password: text
+                    })} />
                     <FormValidationMessage>Error message</FormValidationMessage>
-                    <TouchableOpacity onPress = {()=>Actions.home()}><Text>Submit</Text></TouchableOpacity>
+                    <TouchableOpacity onPress={(() => this.signUp)}><Text>Sign Up</Text></TouchableOpacity>
                 </View>
             </ScrollView>
         )
