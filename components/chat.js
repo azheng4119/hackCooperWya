911f1030abef0753e5109ec49fc2a8bfaa76c31e
azheng4119 <azheng4119@bths.edu>
2019-09-07 05:04:47 -0400

    chat

--- components/chat.js
+++ components/chat.js
@@ -2,37 +2,53 @@ import React from 'react'
 import Login from '../components/login'
 import io from 'socket.io-client'
 import { YellowBox } from 'react-native';
-import { StyleSheet, View, ScrollView, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, ScrollView, Text, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
 import { FormLabel, FormInput, FormValidationMessage, Card, Icon } from 'react-native-elements'
 import { CardItem } from 'native-base';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { connect } from "react-redux";
+import { getUserThunk } from '../store/utilities/currentuser';
 
 
-export default class chat extends React.Component {
+class chat extends React.Component {
+    _isMounted = false;
     constructor(props) {
         super();
         this.state = {
-            chats: []
+            chats: [],
+            message: ""
+
         }
     }
 
     componentDidMount() {
+        this._isMounted = true;
         console.ignoredYellowBox = ['Remote debugger'];
         YellowBox.ignoreWarnings([
             'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
         ]);
         this.socket = io('http://wya-api.herokuapp.com/');
         this.socket.on("chat message", msg => {
-            this.setState({
-                chats: [...this.state.chats, msg]
-            })
+            if (this._isMounted) {
+                this.setState({
+                    chats: [...this.state.chats, msg]
+                })
+            }
         })
     }
-
+    componentWillUnmount(){
+        this._isMounted = false;
+        this.props.getUser();
+    }
     sendMessage = (msg) => {
-        this.socket.emit("chat message", msg);
-        this.setState({
-            chats: [...this.state.chats, msg]
-        })
+        let mess = this.props.user.user.firstname+ " " +this.props.user.user.lastname+": "+msg
+        this.socket.emit("chat message", mess);
+        if (this._isMounted) {
+            this.setState({
+                chats: [...this.state.chats, mess],
+                message: ""
+            })
+        }
     }
     render() {
         let messages = this.state.chats.map((message, i) => {
@@ -43,17 +59,38 @@ export default class chat extends React.Component {
             )
         })
         return (
-            <View style={{ display: 'flex', flexDirection: 'column', width :'100%', paddingBottom : 20 }}>
-                <ScrollView style={{ padding : 10,alignSelf: 'center', maxHeight: 200, width: '70%',backgroundColor: 'white', borderRadius: 15 }}>{messages}</ScrollView>
-                <Card>
-                    <CardItem>
-                            <Icon name= "chat"></Icon>
-                            <FormInput inputStyle={{ width: 200 }}></FormInput>
-                            <TouchableOpacity onPress={() => this.sendMessage('yoooo')}><Text style = {{color:'blue'}}>Send</Text></TouchableOpacity>
-                    </CardItem>
-                </Card>
-            </View>
+            <KeyboardAvoidingView behavior="padding" enabled style={{ width: '100%' }}>
+                <View style={{ display: 'flex', flexDirection: 'column', width: '100%', paddingBottom: 20 }}>
+                    <ScrollView style={{ padding: 10, alignSelf: 'center', maxHeight: 200, width: '70%', backgroundColor: 'white', borderRadius: 15 }}>{messages}</ScrollView>
+                    <Card>
+                        <CardItem>
+                            <Icon name="chat"></Icon>
+                            <FormInput onChangeText={(text) => this.setState({
+                                message: text
+                            })}
+                                onSubmitEditing={() => this.sendMessage(this.state.message)}
+                                inputStyle={{ width: 200 }}
+                                value={this.state.message}></FormInput>
+                            <TouchableOpacity title="SUBMIT" onPress={() => this.sendMessage(this.state.message)}><Text style={{ color: 'blue' }}>Send</Text></TouchableOpacity>
+                        </CardItem>
+                    </Card>
+                </View>
+            </KeyboardAvoidingView>
         )
 
     }
 }
+
+const mapState = (state) => {
+    return {
+        user: state.user
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return {
+        getUser: () => dispatch(getUserThunk())
+    }
+}
+
+export default connect(mapState, mapDispatch)(chat);
